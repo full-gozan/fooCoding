d358b6a47ce951ac7c2643ce899b2c328dc17fb8
ahmed gozan <ahmedgozan@yahoo.com>
2019-08-12 00:15:31 +0200

    arrange the folders

---
+++ javascript/week1/README.md
@@ -0,0 +1,1 @@
+#learn how to work on a reposotory with more than one developer by create a branches
---
+++ javascript/week2/main.js
@@ -0,0 +1,78 @@
+var x;
+console.log("the value of my variable x will be: my best friends");
+console.log(x);
+x=3;
+console.log("the value of my variable x will be: my favourite number");
+console.log(x);
+
+var y;
+console.log("the value of my variable x will be: my best object");
+console.log(y);
+y="husam";
+console.log("the value of my variable x will be: my favourite study");
+console.log(y);
+
+
+
+
+var z;
+z=7.25;
+console.log(z);
+var a;
+a=Math.round(z);
+if (a>z)
+console.log(a);
+else console.log(z);
+
+var plurality;
+console.log("this array contains my favourite animals");
+console.log(plurality);
+plurality=["lions", "tigers", "bear"];
+console.log(plurality);
+plurality.unshift("baby pig");
+console.log(plurality);
+
+
+let myString = "this is a test"
+console.log(myString);
+console.log(myString.length);
+
+let right =true;
+let int =7;
+let strg ="world";
+let arry= [strg, int,right];
+console.log(right);
+console.log(int);
+console.log(strg);
+console.log(arry);
+console.log(right+ " is boolean data type");
+console.log(int+ " is intiger  data type");
+console.log(strg+ " is string data type");
+console.log(arry+ " is array data type");
+console.log(typeof right);
+console.log(typeof int);
+console.log(typeof strg);
+console.log(typeof arry);
+if (int = Number ){
+    console.log("they are same");
+
+}
+else console.log("they are differant");
+
+var x= 7;
+x= x % 3;
+var b= x % 4;
+var c = x % 9;
+console.log(x);
+console.log(b);
+console.log(c);
+
+var multiple =["honey", 4 , ["orange", null, 4]];
+var inf = 6/0;
+if (6/0 === 10/0)
+console-log("Infinity works");
+else console-log("Infinity does not work");
+
+
+
+
