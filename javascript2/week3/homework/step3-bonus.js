b61b9d9a5c84c1dbc8ba68ad234e5670a689da73
ahmed gozan <ahmedgozan@yahoo.com>
2019-09-10 14:19:54 +0200

    solve step3 and step3-bonus homework

--- javascript2/week3/homework/step3-bonus.js
+++ javascript2/week3/homework/step3-bonus.js
@@ -1,14 +1,20 @@
 'use strict';
 
-const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
+let values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
 
 function makeUnique(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  const fliterArray = () => {
+let temparr =[];
+temparr=[...new Set(arr)]
+ return temparr
+  }
+  return fliterArray()
 }
 
 const uniqueValues = makeUnique(values);
 console.log(uniqueValues);
+
+
 
 // Do not change or remove anything below this line
 module.exports = makeUnique;
