0a866fab36fd80f8ecf02fa471fe51fec8693661
full-gozan <51009204+full-gozan@users.noreply.github.com>
2019-11-16 00:20:10 +0100

    Update index.js

--- node.js/week2/homework/src/index.js
+++ node.js/week2/homework/src/index.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// TODO: Write the homework code in this file
+const list = {
+    help: 'show the help commands',
+    list: 'list the to dos',
+    add: 'add to to do list',
+    remove: 'remove one or more from the list',
+    reset: 'reset all the items in the to dos list'
+}
+
+let toDosList = ['go', 'come', 'buy'];
+
+if (process.argv[2] === 'help' || !process.argv[2]) {
+    console.log(list);
+}
+else if (process.argv[2] === 'list') {
+    console.log(toDosList);
+}
+else if (process.argv[2] === 'add') {
+    toDosList.push(process.argv[3]);
+    console.log(toDosList);
+}
+else if (process.argv[2] === 'remove') {
+    toDosList.splice(process.argv[3], 1);
+    console.log(toDosList);
+}
+else if (process.argv[2] === 'reset') {
+    toDosList = [];
+    console.log('To dos is empty');
+}
