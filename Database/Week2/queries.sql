837438b07752e78c6f07731b26b193a178a692d5
full-gozan <ahmedgozan@yahoo.com>
2019-12-15 23:32:18 +0100

    add week2

---
+++ Database/Week2/queries.sql
@@ -0,0 +1,49 @@
+prepare get_capital from 
+'
+SELECT city.Name FROM city
+inner join country on
+country.Code = city.CountryCode AND country.Capital = city.ID
+
+where country.Name = ?';
+set @land = 'IRAQ';
+execute get_capital using @land;
+
+
+/* List all the languages spoken in the region Y (Accept Y from user)*/ 
+use new_world;
+prepare get_language from 
+"SELECT Language FROM countrylanguage inner join country on country.Code = countrylanguage.CountryCode where country.Region = ?";
+set @region = 'Middle East';
+execute get_language using @region;
+
+/* Find the number of cities in which language Z is spoken (Accept Z from user)*/
+use new_world;
+prepare get_cities_numbers from 
+"select count(city.id) from city inner join countrylanguage on city.countrycode = countrylanguage.countrycode where countrylanguage.language = ?;";
+set @lang = 'Arabic';
+execute get_cities_numbers using @lang;
+
+/* List all the continents with the number of languages spoken in each continent*/
+ select country.continent, count(distinct(countrylanguage.language))as languages_number from country inner join countrylanguage on country.code = countrylanguage.countrycode group by country.continent;
+
+/* Are there any countries that have
+
+Same official language
+Same region
+If yes, display those countries.
+If no, display TRUE or FALSE */
+DELIMITER //
+ CREATE PROCEDURE commen_countries_with_language(
+in lang VARCHAR(255),
+in reg VARCHAR(255)
+)
+BEGIN
+select c.name, l.language from country c
+inner join new_world.countrylanguage l on c.code = l.countrycode
+where l.isofficial = "T"
+and c.region = reg
+and l.language = lang;
+    
+END //
+DELIMITER;
+call commen_countries_with_languag('Arabic','Middle East');
---
+++ Database/Week2/queries_on_new_world.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'CelineAhmed2018',
+  database: 'new_world',
+});
+
+//connection to database
+mysqlConnection.connect(function (err){
+    if(err){
+        console.log('failed to connect to the database', err);
+        
+    }
+    else{
+        console.log("connected to the database!");
+        
+    }
+})
+//run set of queries 
+const queries=[
+    'SELECT city.Name FROM city inner join country on country.Code = city.CountryCode AND country.Capital = city.ID where country.Name ="Iraq"',
+    'SELECT Language FROM countrylanguage inner join country on country.Code = countrylanguage.CountryCode where country.Region = "Middle East"',
+    'select count(city.id) from city inner join countrylanguage on city.countrycode = countrylanguage.countrycode where countrylanguage.language = "Arabic"',
+    'select country.continent, count(distinct(countrylanguage.language))as languages_number from country inner join countrylanguage on country.code = countrylanguage.countrycode group by country.continent;'
+
+].map (query=>mysqlConnection.query(
+    query,(mess)=>{
+        index++;
+                console.log('succeeded! the '+ index+'th query was added correctly');
+            }
+    )            
+)
+//call the Procedure 
+let callProcedure = CALL 'getALLcountries(?,?)';
+ 
+connection.query(callProcedure,'Arabic','Middle East', (error, results, fields) => {
+  if (error) {
+    return console.error(error.message);
+  }
+});
+mysqlConnection.end();
